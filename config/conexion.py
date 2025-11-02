504427de3b3fbcd636049a2c6dd804bb67ab262a
Alexandra-cod9 <00091421@uca.edu.sv>
2025-11-01 20:29:34 -0600

    Create conexion.py

---
+++ config/conexion.py
@@ -0,0 +1,19 @@
+import mysql.connector
+from mysql.connector import Error
+
+def obtener_conexion():
+    try:
+        conexion = mysql.connector.connect(
+            host='Tbgdcfmb1arijnmihxnub-mysql.services.clever-cloud.com',
+            user='Tup0crtddmf4aoous',
+            password='mkHalSz2YPOtR8jya8DS',
+            database='bgdcfmb1arijnmihxnub',
+            port=3306
+        )
+        if conexion.is_connected():
+            return conexion
+        else:
+            return None
+    except Error as e:
+        print(f"Error al conectar: {e}")
+        return None
